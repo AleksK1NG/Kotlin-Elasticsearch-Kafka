64d5a63cdf57a16406ed06dd0ae98d8a435dd501
Alexander <alexander.bryksin@yandex.ru>
2022-08-20 16:36:31 +0300

    feature: add http requests validation

--- src/main/kotlin/com/search/microservice/dto/IndexProductRequest.kt
+++ src/main/kotlin/com/search/microservice/dto/IndexProductRequest.kt
@@ -1,9 +1,12 @@
 package com.search.microservice.dto
 
+import javax.validation.constraints.Min
+import javax.validation.constraints.Size
+
 data class IndexProductRequest(
-    val title: String,
-    val description: String,
-    val imageUrl: String,
-    val countInStock: Long,
-    val shop: String,
+    @get:Size(min = 6, max = 60) val title: String,
+    @get:Size(min = 6, max = 5000) val description: String,
+    @get:Size(min = 6, max = 1000) val imageUrl: String,
+    @get:Min(1) val countInStock: Long,
+    @get:Size(min = 6, max = 60) val shop: String,
 )
