fd3ae14ccddb4633c691844194883cf78fe6e51a
Alexander <alexander.bryksin@yandex.ru>
2022-08-20 13:34:37 +0300

    feature: add repository bulk insert method

--- src/main/kotlin/com/search/microservice/repository/ProductElasticRepository.kt
+++ src/main/kotlin/com/search/microservice/repository/ProductElasticRepository.kt
@@ -2,8 +2,10 @@ package com.search.microservice.repository
 
 import com.search.microservice.domain.Product
 import com.search.microservice.utils.PaginationResponse
+import java.util.concurrent.BlockingDeque
 
 interface ProductElasticRepository {
     suspend fun index(product: Product)
     suspend fun search(term: String, page: Int, size: Int): PaginationResponse<Product>
+    suspend fun bulkInsert(products: BlockingDeque<Product>)
 }
